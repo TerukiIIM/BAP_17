74beb7e90991ec6e7906b9f6c0831e1ff0d5261b
Nathan Bo <nathan.github22@gmail.com>
2025-03-16 14:18:20 +0100

    feat: adding tasks game component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import './index.css'
-import { motion } from "motion/react"
+import TasksGame from './components/TasksGame'
 
 function App() {
 
   return (
     <>
-      <div className='flex justify-center items-center gap-10 h-screen'>
-        {/* Test tailwind Css */}
-        <h1 className='bg-red-500'>Test</h1>
-        {/* Test motion */}
-        <motion.div
-          animate={{ rotate: 360 }} 
-          transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
-          className='size-32 bg-zinc-900' />
-      </div>
+      <TasksGame />
     </>
   )
 }
